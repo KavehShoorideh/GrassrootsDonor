20be8710acce225a4233ddf5dda0da6232820e51
Kaveh Shoorideh <kaveh.shoorideh@gmail.com>
2019-10-04 13:12:58 -0700

    turned debug mode off.

--- politimpact/run_flask.py
+++ politimpact/run_flask.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 from politimpact.flaskserver import app
-app.run(debug = True)
+# app.run(debug = True)
+app.run(
+    host = '127.0.0.1',
+    port = 5000
+)
 
 # from update_candidate_data import update_candidate_data as updater
 # TODO: Add script to run data updater() every midnight
